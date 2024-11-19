5ff77ca835a1f850db0eceb3672858221506772f
EduMss <eduardo555gatinho@hotmail.com>
2024-11-19 15:38:53 -0300

    Corrigindo o ItemMove

--- src/ItemMove/index.tsx
+++ src/ItemMove/index.tsx
@@ -2,35 +2,38 @@ import React, {useState} from "react";
 import './styleItemMove.css';
 
 export default function ItemMove() {
-    const [showMove, setShowMove] = useState(false);
-    const [isAnimating, setIsAnimating] = useState(false); // Estado para controlar a animação de saída
+    const [isAtivos, setIsAtivos] = useState(false); // Estado para controlar a animação de saída
+    const [isAtivos2, setIsAtivos2] = useState(false); // Estado para controlar a animação de saída
 
     const toggleMove = () => {
-        if (showMove) {
+        if (isAtivos) {
             // Se já estiver visível, define a animação de saída
-            setIsAnimating(true);
-            setTimeout(() => {
-                setShowMove(false);
-                setIsAnimating(false);
-            }, 600); // Tempo em milissegundos igual ao da animação de saída
+            setIsAtivos(false);
         } else {
             // Exibe a div imediatamente quando deve aparecer
-            setShowMove(true);
+            setIsAtivos(true);
+        }
+    };
+    const toggleMove2 = () => {
+        if (isAtivos2) {
+            // Se já estiver visível, define a animação de saída
+            setIsAtivos2(false);
+        } else {
+            // Exibe a div imediatamente quando deve aparecer
+            setIsAtivos2(true);
         }
     };
 
     return (
         <div className={`ItemMove`}>
             <div className="NavBar">
-                <button onClick={toggleMove}>{showMove ? "Ocultar" : "Aparecer"}</button>
+                <button onClick={toggleMove}>{isAtivos ? "Ocultar" : "Aparecer"}</button>
+                <button onClick={toggleMove2}>{isAtivos2 ? "Ocultar2" : "Aparecer2"}</button>
             </div>
             <div className={`AreaMove`}>
-                {showMove && (
-                    <div className={`Move ${isAnimating ? 'slide-out' : 'slide-in'}`}></div>
-                )}
-                <div className="RestoTela">
-
-                </div>
+                <div className={`Move ${isAtivos ? 'ativo' : ''}`}></div>
+                <div className={`RestoTela`}></div>
+                <div className={`Move2 ${isAtivos2 ? 'ativo' : ''}`}></div>
             </div>
         </div>
     );
